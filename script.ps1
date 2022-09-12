00994bde6176ad88194534d339f2627b9ea99652
Lorenzo Zaccomer <80517436+lorenzozaccomer@users.noreply.github.com>
2022-09-12 13:07:42 +0200

    Add script.ps1

---
+++ script.ps1
@@ -0,0 +1,8 @@
+Get-AppxPackage
+Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsCalculator | Remove-AppxPackage
+Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
